c61a20a6d51896ac986f456f12f53637cdeb64c8
Annika Kriskovich <annika.krisko2013@gmail.com>
2022-12-22 14:10:04 -0600

    commit tree 3440d5e0137cffab47c868b01709024c01f547e2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+// returnLastTwoDivers
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+// selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier
+const createFareMultiplier = (multiplier) => (fare) => fare * multiplier;
+
+// fareDoubler
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers
+const selectDifferentDrivers = (drivers, SelectFunction) => SelectFunction(drivers);
